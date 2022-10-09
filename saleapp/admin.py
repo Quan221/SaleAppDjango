2eaa812202151c37196b8b1bd36ef45f786cf246
minhquangle <tuilaminhquang970>
2022-10-09 10:22:07 +0700

    Add field status to Receipt Model

--- saleapp/admin.py
+++ saleapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User, Category, Product, Order
+from .models import User, Category, Product, Order, Receipt, OrderDetail
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Receipt)
+
--- saleapp/models.py
+++ saleapp/models.py
@@ -11,10 +11,27 @@ class StatusEnum(Enum):
         return tuple((i.name, i.value) for i in cls)
 
 
+
+class PayMentMethod(Enum):
+    Momo='Momo'
+    COD ='COD'
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
+class Role(Enum):
+    Customer='Customer'
+    Shipper='Shipper'
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
 # Create your models here.
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='uploads/%Y/%m')
-
+    role = models.CharField(max_length=50,choices=Role.choices())
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,8 +39,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
 
 class ModelBase(models.Model):
     active = models.BooleanField(default=True)
@@ -48,7 +63,6 @@ class Order(models.Model):
     ship_address = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=50,choices=StatusEnum.choices())
 
 class OrderDetail(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -61,6 +75,8 @@ class OrderDetail(models.Model):
 class Receipt(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    status = models.CharField(max_length=30,choices=StatusEnum.choices())
+    payment_method = models.CharField(max_length=30,choices=PayMentMethod.choices())
 
 
 
